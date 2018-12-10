07f3eb9cfaca20531d3fe94960c951f4a7a0a9e2
richard065211 <zhangzhenzhen1999@163.com>
2018-12-10 21:26:03 +0800

    高德地图API

---
+++ app/src/main/java/com/example/richard/gaodemapapi/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.richard.gaodemapapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.MyLocationStyle;
+
+public class MainActivity extends AppCompatActivity{
+    MapView mMapView;
+    AMap aMap;
+    MyLocationStyle myLocationStyle;
+
+    public AMapLocationClient mLocationClient = null;  //声明AMapLocationClient类对象
+    public AMapLocationListener mLocationListener;  //声明定位回调监听器
+    public AMapLocationClientOption mLocationOption = null; //声明AMapLocationClientOption对象
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+        initMap();
+        initMyLocation();
+    }
+    /*
+    *初始化地图
+    * */
+    private void initMap() {
+        if(aMap==null) {
+            aMap = mMapView.getMap();
+        }
+        myLocationStyle = new MyLocationStyle();  //初始化定位蓝点样式类
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。
+        myLocationStyle.interval(2000);//设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);  //设置定位蓝点的Style
+        aMap.setMyLocationEnabled(true);
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW);
+    }
+
+    /*
+    * 初始化定位到当前位置
+    * */
+    private void initMyLocation() {
+        mLocationListener=new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                aMapLocation.getLatitude();//获取纬度
+                aMapLocation.getLongitude();//获取经度
+                aMapLocation.getLocationType(); //获取定位来源
+                StringBuffer buffer = new StringBuffer();
+                buffer.append(aMapLocation.getLatitude()+"纬度"+aMapLocation.getLongitude()+"定位来源"+aMapLocation.getLocationType()
+                );
+                Toast.makeText(getApplicationContext(),buffer.toString(),Toast.LENGTH_LONG).show();
+            }
+        };
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy); //设置定位模式为高精度模式
+        mLocationOption.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+        if(mLocationClient!=null){
+            mLocationClient.setLocationOption(mLocationOption);
+            //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+            mLocationClient.stopLocation();
+            mLocationClient.startLocation();
+        }
+        mLocationClient.startLocation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+}
